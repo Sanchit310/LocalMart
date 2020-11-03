dc61424cf304f302dd3375dfb62203352a1bb028
Sanchit310 <44144836+Sanchit310@users.noreply.github.com>
2020-11-03 13:13:00 +0530

    Initial commit

---
+++ app/src/main/java/example/android/localmart/data/model/Store.kt
@@ -0,0 +1,6 @@
+package example.android.localmart.data.model
+
+class Store(private val storeName :  String, private val storeType : String,
+            private val storeRating : Int , private val storeDistance : Int, private val storeImageUrl : String){
+
+}
