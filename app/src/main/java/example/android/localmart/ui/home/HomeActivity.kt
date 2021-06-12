2563a36fe544cc5458768f7359862cafe45adc6f
Sanchit310 <vermasanchit32@gmail.com>
2021-06-12 12:13:15 +0530

    items added

--- app/src/main/java/example/android/localmart/ui/home/HomeActivity.kt
+++ app/src/main/java/example/android/localmart/ui/home/HomeActivity.kt
@@ -1,4 +1,4 @@
-package example.android.localmart.ui.home
+ package example.android.localmart.ui.home
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
---
+++ app/src/main/java/example/android/localmart/ui/signup/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package example.android.localmart.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import example.android.localmart.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
